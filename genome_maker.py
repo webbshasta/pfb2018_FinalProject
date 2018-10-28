f7915a2c4eda2088e8ab3ca0bc7916b254ba8b7d
crisPFB <cdesanti@cshl.edu>
2018-10-28 17:27:50 +0000

    genome maker update

--- genome_maker.py
+++ genome_maker.py
@@ -4,25 +4,11 @@
 # Defining function for starter genome
 #==============
 
-# This function requires the Veeple1 and Veeple2 dictionaries. These dictionaries will have all keys filled
+# This function requires the Veeple1 and Veeple2 dictionaries. These 
+dictionaries will have all keys filled
 # except for the 'mGenome' and the 'pGenome'.
 
 import random
-
-# Alleles score
-
-#AlleleA_fit_dict = {
-                    #'A1': '5'
-                    #'A2': '2'
-                    #'A3': '-1'
-                    #'A4': '3'
-                    #}
-#AlleleB_fit_dict = {
-                    #'B1': '0'
-                    #'B2': '1'
-                    #'B3': '-2'
-                    #'B4': '4'
-                    #}
 
 # Eve
 Veeple1 = {
@@ -46,6 +32,33 @@ Veeple2 = {
 
 allelesGeneA = ['A1', 'A2', 'A3', 'A4']
 allelesGeneB = ['B1', 'B2', 'B3', 'B4']
+# Alleles score
+# Defining base fitness based on alleles score
+
+Allele_fit_dict = {
+                    'A1':5,
+                    'A2':2,
+                    'A3':1,
+                    'A4':3,
+                    'B1':0,
+                    'B2':1,
+                    'B3':-2,
+                    'B4':4
+                    }
+def base_fitness(Veeple):
+    mGenome = Veeple['mGenome']
+    pGenome = Veeple['pGenome']
+    base_fitness = 0
+    for allele in mGenome:
+        if allele in Allele_fit_dict:
+            base_fitness += Allele_fit_dict[allele]
+    for allele in pGenome:
+        if allele in Allele_fit_dict:
+            base_fitness += Allele_fit_dict[allele]
+
+    return base_fitness
+BF = base_fitness(Veeple1)
+print(BF)
 
 def starterGenome(alleles1, alleles2, Veeple):
     randomIndex1 = random.randint(0, len(alleles1) -1)
@@ -56,6 +69,9 @@ def starterGenome(alleles1, alleles2, Veeple):
     Veeple['pGenome'].append(alleles1[randomIndex2])
     Veeple['mGenome'].append(alleles2[randomIndex3])
     Veeple['pGenome'].append(alleles2[randomIndex4])
+    Base_Fitness = 0
+    Base_Fitness = base_fitness(Veeple)
+    Veeple['Base Fitness'] = Base_Fitness
 
     return(Veeple)
 
