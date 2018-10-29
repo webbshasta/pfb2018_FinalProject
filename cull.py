ce79b1fd19a3d1d110c1f96bd02dba590dcbd683
mfeuerman <mfe@ip-172-31-44-163.us-east-2.compute.internal>
2018-10-29 01:58:10 +0000

    updates for linking to the backbone script

--- cull.py
+++ cull.py
@@ -3,46 +3,46 @@
 #import VeepleCensus
 
 # Eve
-Veeple1 = {
-          'ID':1,
-          'Sex':'X',
-          'mGenome':[],
-          'pGenome':[],
-          'Base Fitness':10,
-          'Fitness':10
-          }
+#Veeple1 = {
+#         'ID':1,
+#         'Sex':'X',
+#         'mGenome':[],
+#        'pGenome':[],
+#         'Base Fitness':10,
+#         'Fitness':10
+#         }
 
 # Adam
-Veeple2 = {
-          'ID':2,
-          'Sex':'Y',
-          'mGenome':[],
-          'pGenome':[],
-          'Base Fitness':10,
-          'Fitness':10
-          }
+#Veeple2 = {
+#         'ID':2,
+#         'Sex':'Y',
+#         'mGenome':[],
+#         'pGenome':[],
+#         'Base Fitness':10,
+#         'Fitness':10
+#         }
 
-Veeple3 = {
-          'ID':3,
-          'Sex':'X',
-          'mGenome':[],
-          'pGenome':[],
-          'Base Fitness':10,
-          'Fitness':4
-          }
-
-
-Veeple4 = {
-          'ID':4,
-          'Sex':'Y',
-          'mGenome':[],
-          'pGenome':[],
-          'Base Fitness':10,
-          'Fitness':1
-          }
+#Veeple3 = {
+#         'ID':3,
+#         'Sex':'X',
+#         'mGenome':[],
+#         'pGenome':[],
+#         'Base Fitness':10,
+#         'Fitness':4
+#         }
 
 
-VeepleCensus = [Veeple1, Veeple2, Veeple3, Veeple4]
+#Veeple4 = {
+#         'ID':4,
+#         'Sex':'Y',
+#         'mGenome':[],
+#         'pGenome':[],
+#         'Base Fitness':10,
+#         'Fitness':1
+#         }
+
+
+#VeepleCensus = [Veeple1, Veeple2, Veeple3, Veeple4]
 
 def veeplecull(VeepleCensus):
     for Veeple in VeepleCensus:
@@ -52,6 +52,6 @@ def veeplecull(VeepleCensus):
             VeepleCensus.remove(Veeple)
 
     return VeepleCensus
-print(len(VeepleCensus))
+#print(len(VeepleCensus))
 veeplecull(VeepleCensus)
-print(len(VeepleCensus))
+#print(len(VeepleCensus))
--- fitness.py
+++ fitness.py
@@ -3,24 +3,24 @@
 
 
 
-Veeple1 = {
-          'ID' : 1,
-          'Sex' : 'X',
-          'mGenome' : ['A1','B1'],
-          'pGenome' : ['A1','B1'],
-          'Base Fitness' : 10,
-          'Fitness' : 10
-          }
+#Veeple1 = {
+ #         'ID' : 1,
+ #        'Sex' : 'X',
+#        'mGenome' : ['A1','B1'],
+#         'pGenome' : ['A1','B1'],
+#         'Base Fitness' : 10,
+#         'Fitness' : 10
+#         }
 
-Envir = -2
-Dis = -3
+#Envir = -2
+#Dis = -3
 
-def Veepfit(Veeple1, Envir, Dis): #set up function
+def Veepfit(Veeple1, Envir, Nat Dis,  Dis): #set up function
 
-    Veepfit = int(Veeple1['Fitness']) + int(Envir) + int(Dis) #sum up all the contributions
+    Veepfit = int(Veeple1['Fitness']) + int(Envir) + int(NatDis) + int(Dis) #sum up all the contributions
     Veeple1['Fitness'] = Veepfit  #add back to dictionary
-    print(Veepfit, Veeple1['Fitness'])
-    return Veepfit #calculated answer 
+    #print(Veepfit, Veeple1['Fitness'])
+    return Veeple1 #calculated answer 
 
     # print(Veepfit, Veeple1['Fitness'])
-Veepfit(Veeple1, Envir, Dis)
+Veepfit(Veeple1)
