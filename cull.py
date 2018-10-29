80a9bb875baca75e4e458ec2827762b46cb3613b
Shasta Webb <webbshasta@gmail.com>
2018-10-29 09:38:08 -0400

    re=adding cull

---
+++ cull.py
@@ -0,0 +1,55 @@
+#! usr/bin/env python3
+
+#import VeepleCensus
+
+# Eve
+#Veeple1 = {
+#         'ID':1,
+#         'Sex':'X',
+#         'mGenome':[],
+#        'pGenome':[],
+#         'Base Fitness':10,
+#         'Fitness':10
+#         }
+
+# Adam
+#Veeple2 = {
+#         'ID':2,
+#         'Sex':'Y',
+#         'mGenome':[],
+#         'pGenome':[],
+#         'Base Fitness':10,
+#         'Fitness':10
+#         }
+
+#Veeple3 = {
+#         'ID':3,
+#         'Sex':'X',
+#         'mGenome':[],
+#         'pGenome':[],
+#         'Base Fitness':10,
+#         'Fitness':4
+#         }
+
+
+#Veeple4 = {
+#         'ID':4,
+#         'Sex':'Y',
+#         'mGenome':[],
+#         'pGenome':[],
+#         'Base Fitness':10,
+#         'Fitness':1
+#         }
+
+
+#VeepleCensus = [Veeple1, Veeple2, Veeple3, Veeple4]
+
+def veeplecull(census):
+    for Veeple in census:
+        if Veeple['Fitness'] < 2:
+           # print(Veeple['ID'],"to be culled")
+            census.remove(Veeple)
+    return census
+#print(len(VeepleCensus))
+#veeplecull(VeepleCensus)
+#print(len(VeepleCensus))
