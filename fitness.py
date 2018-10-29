bc1b10d6171936762cab923b4324d70fd227cabd
Shasta Webb <webbshasta@gmail.com>
2018-10-29 01:00:31 -0400

    updating fitness function

--- fitness.py
+++ fitness.py
@@ -15,12 +15,12 @@
 #Envir = -2
 #Dis = -3
 
-def Veepfit(Veeple1, Envir, Nat Dis,  Dis): #set up function
+def Veepfit(Veeple1, envScore, natDisScore, diseaseScore): #set up function
 
-    Veepfit = int(Veeple1['Fitness']) + int(Envir) + int(NatDis) + int(Dis) #sum up all the contributions
+    Veepfit = int(Veeple1['Fitness']) + int(envScore) + int(natDisScore) + int(diseaseScore) #sum up all the contributions
     Veeple1['Fitness'] = Veepfit  #add back to dictionary
     #print(Veepfit, Veeple1['Fitness'])
-    return Veeple1 #calculated answer 
+    return Veeple1 #calculated answer
 
     # print(Veepfit, Veeple1['Fitness'])
-Veepfit(Veeple1)
+#eepfit(Veeple1)
