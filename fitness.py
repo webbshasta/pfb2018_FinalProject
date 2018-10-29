9a7b0ef447aa8d65490406ec3df0ac3eec71a173
Shasta Webb <webbshasta@gmail.com>
2018-10-29 01:07:07 -0400

    updating penalities

--- fitness.py
+++ fitness.py
@@ -17,7 +17,7 @@
 
 def Veepfit(Veeple1, envScore, natDisScore, diseaseScore): #set up function
 
-    Veepfit = int(Veeple1['Fitness']) + int(envScore) + int(natDisScore) + int(diseaseScore) #sum up all the contributions
+    Veepfit = int(Veeple1['Fitness']) - int(envScore) - int(natDisScore) - int(diseaseScore) #sum up all the contributions
     Veeple1['Fitness'] = Veepfit  #add back to dictionary
     #print(Veepfit, Veeple1['Fitness'])
     return Veeple1 #calculated answer
