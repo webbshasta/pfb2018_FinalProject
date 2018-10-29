6e153b2bb522dbafdb75ecda81d39b6dcade838c
scottfriedland <sfr@ip-172-31-44-163.us-east-2.compute.internal>
2018-10-29 14:59:19 +0000

    gen mak

--- genome_maker.py
+++ genome_maker.py
@@ -73,7 +73,8 @@ def base_fitness(Veeple):
     return base_fitness
 
 #print(BF)
-
+mGenome = []
+pGenome = []
 def starterGenome(alleles1, alleles2, alleles3, alleles4, alleles5,Veeple):
     randomIndex1 = random.randint(0, len(alleles1) -1)
     randomIndex2 = random.randint(0, len(alleles2) - 1)
@@ -85,16 +86,10 @@ def starterGenome(alleles1, alleles2, alleles3, alleles4, alleles5,Veeple):
     randomIndex8 = random.randint(0, len(alleles4) -1)
     randomIndex9 = random.randint(0, len(alleles5) -1)
     randomIndex10 = random.randint(0, len(alleles5) -1)
-    Veeple['mGenome'].append(alleles1[randomIndex1])
-    Veeple['pGenome'].append(alleles1[randomIndex2])
-    Veeple['mGenome'].append(alleles2[randomIndex3])
-    Veeple['pGenome'].append(alleles2[randomIndex4])
-    Veeple['mGenome'].append(alleles3[randomIndex5])
-    Veeple['pGenome'].append(alleles3[randomIndex6])
-    Veeple['mGenome'].append(alleles4[randomIndex7])
-    Veeple['pGenome'].append(alleles4[randomIndex8])
-    Veeple['mGenome'].append(alleles5[randomIndex9])
-    Veeple['pGenome'].append(alleles5[randomIndex10])
+    mGenome = [alleles1[randomIndex1], alleles2[randomIndex3], alleles3[randomIndex5], alleles4[randomIndex7], alleles5[randomIndex9]] 
+    pGenome = [alleles1[randomIndex2], alleles2[randomIndex4], alleles3[randomIndex6], alleles4[randomIndex8], alleles5[randomIndex10]]
+    Veeple['mGenome'] = mGenome
+    Veeple['pGenome'] = pGenome
     Base_Fitness = base_fitness(Veeple)
     Veeple['Base Fitness'] = Base_Fitness
     Veeple['Fitness'] = Base_Fitness
